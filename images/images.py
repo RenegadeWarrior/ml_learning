03ee651f1cb555819bbe293d0b8c94d697f61a6e
RenegadeWarrior <ajsk201@gmail.com>
2018-07-28 16:48:41 +0530

    upto c25 uploaded

--- images/images.py
+++ images/images.py
@@ -1,12 +1,14 @@
 import numpy as np
 from PIL import Image
-imagelist=[];
-width=60
-height=60
-image_Number=15
-for i in range(1,image_Number+1):
-    image=Image.open("c"+str(i)+".jpg")
-    image_Resized=image.resize((width,height))
-    imagelist.append(np.array(image_Resized))
-Y=np.array([1,1,1,1,1,1,1,1,1,0,0,0,0,0,0])
-images=np.array(imagelist)
+def imageArray():
+    imagelist=[];
+    width=60
+    height=60
+    Image_Number=25
+    for i in range(1,Image_Number+1):
+        image=Image.open("c"+str(i)+".jpg")
+        image_Resized=image.resize((width,height))
+        imagelist.append(np.array(image_Resized))
+    Y=np.array([1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
+    images=np.array(imagelist)
+    return images,Image_Number
